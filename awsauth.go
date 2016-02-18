f622d4f7dbff1289b9474642e7a36b681c552401
Daniel Lamando <me@danopia.net>
2016-02-17 17:25:17 -0800

    AWS ElasticSearch Service uses v4 signing

--- awsauth.go
+++ awsauth.go
@@ -201,6 +201,7 @@ var (
 		"elasticmapreduce":     4,
 		"elastictranscoder":    4,
 		"elasticache":          2,
+		"es":                   4,
 		"glacier":              4,
 		"kinesis":              4,
 		"redshift":             4,
